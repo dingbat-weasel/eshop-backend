a4804bd03a0d1c141601ca55ce554e2fbe6b77d3
Isaac Perk <97003977+isaacperk@users.noreply.github.com>
2023-04-03 13:43:59 -0700

    added Tag Model columns

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,19 +1,17 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 class ProductTag extends Model {}
 
 ProductTag.init(
-  {
-    // define columns
-  },
+  {},
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: "product_tag",
   }
 );
 
