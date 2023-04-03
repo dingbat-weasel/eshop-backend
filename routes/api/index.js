ca489591e137352eae42bb293048c7ddd5f3a3a1
Isaac Perk <97003977+isaacperk@users.noreply.github.com>
2023-04-03 16:06:31 -0700

    near final commit, organized comments

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,24 @@
-const router = require('express').Router();
-const categoryRoutes = require('./category-routes');
-const productRoutes = require('./product-routes');
-const tagRoutes = require('./tag-routes');
+const router = require("express").Router();
+const categoryRoutes = require("./category-routes");
+const productRoutes = require("./product-routes");
+const tagRoutes = require("./tag-routes");
 
-router.use('/categories', categoryRoutes);
-router.use('/products', productRoutes);
-router.use('/tags', tagRoutes);
+router.use("/categories", categoryRoutes);
+router.use("/products", productRoutes);
+router.use("/tags", tagRoutes);
 
 module.exports = router;
+
+/*
+req.body for testing put/post routes
+{
+      "product_name": "Baseball",
+      "price": 200.00,
+      "stock": 3,
+      "tagIds": [1, 2, 3, 4]
+    }
+OR
+{
+		"category_name": "Shoes"
+}
+*/
